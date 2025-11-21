c2a12b7ac2bd5b9a30058a4201d90bf14c1d802e
Anas <anas2004@live.co.uk>
2025-11-21 17:41:59 +0100

    Add PrivateInfo.example.cs for database connection

---
+++ WPFCSDProject/Services/PrivateInfo.example.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFCSDProject.Services
+{
+    // This is an example file showing how to structure PrivateInfo.cs
+    // When making changes to your own PrivateInfo.cs file, update the 
+    // template so that other developers have a reference
+    internal class PrivateInfoExample
+    {
+        public const string ConnectionString =
+            "Server=SERVER ADDRESS;" +
+            "Port=3306;" +
+            "Database=WS374402_CSD;" +
+            "Uid=USERNAME;" +
+            "Pwd=PASSWORD;";
+    }
+}
