773476f50d0ea7953276527e8378ead3f31e5d42
kirasokoya <kirasokoya@gmail.com>
2025-11-21 14:13:00 +0000

    Renamed class for the example Private Info class to avoid having two classes with the same name

--- WPFCSDProject/Services/PrivateInfo.example.cs
+++ WPFCSDProject/Services/PrivateInfo.example.cs
@@ -7,7 +7,7 @@ namespace WPFCSDProject.Services
     // This is an example file showing how to structure PrivateInfo.cs
     // When making changes to your own PrivateInfo.cs file, update the 
     // template so that other developers have a reference
-    internal class PrivateInfo
+    internal class PrivateInfoExample
     {
         public const string ConnectionString =
             "Server=SERVER ADDRESS;" +
