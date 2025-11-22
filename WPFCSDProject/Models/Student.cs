bcf61354107392c07ed5ec1e8a334979cd07ccc4
kirasokoya <kirasokoya@gmail.com>
2025-11-22 18:46:31 +0000

    feat(models): implement user class hierarchy and role enum

---
+++ WPFCSDProject/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFCSDProject.Models
+{
+    /// <summary>
+    /// This class inherits from user and features properties that are
+    /// only relevant for students
+    /// </summary>
+    public class Student : User
+    {
+        // Maps to 'studentNumber' in the database (unique)
+        public string StudentNumber { get; set; } = string.Empty;
+
+        // Maps to 'courseTitle' in the database
+        public string CourseTitle { get; set; } = string.Empty;
+    }
+}
